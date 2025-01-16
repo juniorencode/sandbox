f302ee406804cf1bdba0e0dd17cb2b2de490d983
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-16 15:28:08 -0500

    feat: install javascript editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import Editor from '@monaco-editor/react';
+
 const App = () => {
-  return <h1 className="text-blue-500">Hello World!</h1>;
+  const [code, setCode] = useState('');
+
+  const handleEditorChange = value => {
+    setCode(value);
+  };
+
+  return (
+    <div className="w-[50vw] h-screen">
+      <Editor
+        defaultLanguage="javascript"
+        theme="vs-dark"
+        value={code}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
 };
 
 export default App;
