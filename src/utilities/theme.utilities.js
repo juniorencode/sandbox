3497ddd7140998b70fec1366e48ab1117a1d839a
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-16 22:49:20 -0500

    refactor: change theme to github dark

---
+++ src/utilities/theme.utilities.js
@@ -0,0 +1,47 @@
+export const githubDarkTheme = {
+  base: 'vs-dark',
+  inherit: false,
+  colors: {
+    'editor.foreground': '#d1d7e0',
+    'editor.background': '#212830',
+    'editorWidget.background': '#212830',
+    'editor.foldBackground': '#293038',
+    'editor.lineHighlightBackground': '#292E36',
+    'editorLineNumber.foreground': '#9198A1',
+    'editorLineNumber.activeForeground': '#d1d7e0',
+    'editorWhitespace.foreground': '#4d5b6a',
+    'editorCursor.foreground': '#d1d7e0',
+    'editor.findMatchBackground': '#867647',
+    'editor.findMatchHighlightBackground': '#867647',
+    'editor.wordHighlightBackground': '#434a55',
+    'editor.wordHighlightBorder': '#56606c',
+    'editor.wordHighlightStrongBackground': '#434a55',
+    'editor.wordHighlightStrongBorder': '#56606c',
+    'editorBracketMatch.background': '#292E36',
+    'editorBracketMatch.border': '#292E36',
+    'editorBracketHighlight.foreground1': '#d1d7e0',
+    'editorBracketHighlight.foreground2': '#d1d7e0',
+    'editorBracketHighlight.foreground3': '#d1d7e0',
+    'editorBracketHighlight.foreground4': '#d1d7e0',
+    'editorBracketHighlight.foreground5': '#d1d7e0',
+    'editorBracketHighlight.foreground6': '#d1d7e0'
+  },
+  rules: [
+    {
+      token: 'number',
+      foreground: '#6CB6FF'
+    },
+    {
+      token: 'comment',
+      foreground: '#9198A1'
+    },
+    {
+      token: 'keyword',
+      foreground: '#f47067'
+    },
+    {
+      token: 'string',
+      foreground: '#96D0FF'
+    }
+  ]
+};
