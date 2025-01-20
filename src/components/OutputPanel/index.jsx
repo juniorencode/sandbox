07f74a402ff6152aed3607f441f11a3d00e13294
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-19 19:35:26 -0500

    refactor: add background image to output panel

--- src/components/OutputPanel/index.jsx
+++ src/components/OutputPanel/index.jsx
@@ -17,8 +17,11 @@ export const OutputPanel = ({
   return (
     <div
       ref={outputRef}
-      className="py-5 px-4 h-[calc(100vh-40px)] leading-[1.36] text-[14px] overflow-y-auto overflow-x-hidden text-neutral-300"
-      style={{ width: `calc(${100 - editorSeparator}vw - 10px)` }}
+      className="py-5 px-4 h-[calc(100vh-40px)] leading-[1.36] text-[14px] overflow-y-auto overflow-x-hidden bg-no-repeat bg-[length:50%] bg-center text-neutral-300"
+      style={{
+        width: `calc(${100 - editorSeparator}vw - 10px)`,
+        backgroundImage: `url('/shape.png')`
+      }}
       onScroll={handleScrollOutput}
     >
       <pre className="pb-[calc(100vh-42px)] break-words whitespace-pre-wrap">
